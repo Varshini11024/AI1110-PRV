73c2b9916134d6f662ae5cfee2cb4ead3b7e3f20
varshini11024 <102514254+varshini11024@users.noreply.github.com>
2022-04-16 11:16:26 +0530

    Update code.py

--- Assignment2/codes/code.py
+++ Assignment2/codes/code.py
@@ -1,15 +1,12 @@
-def calc(p_a, p_b, p_a_intersec_b):
-
- p_a_given_b = (p_a_intersec_b)/p_b
- print('P(A|B) =', p_a_given_b)
-
- p_b_given_a = (p_a_intersec_b)/p_a
- print('P(B|A) =', p_b_given_a)
-
-
 #P(A) = 1/2
 #P(B) = 1/3
-#p_a_intersec_b = 1/4
-#finding P(A|B) and P(B|A)
+p_a = 1/2
+p_b = 1/3
+p_a_intersec_b = 1/4
 
-calc(1/2, 1/3, 1/4)
+#finding P(A|B) and P(B|A)
+p_a_given_b = (p_a_intersec_b)/p_b
+print('P(A|B) =', p_a_given_b)
+ 
+p_b_given_a = (p_a_intersec_b)/p_a
+print('P(B|A) =', p_b_given_a)
