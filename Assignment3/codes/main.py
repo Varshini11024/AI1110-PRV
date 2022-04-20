c51ebdb16e38e23098c55f5c4ab6d7d7f0aa75c6
Varshini11024 <102514254+Varshini11024@users.noreply.github.com>
2022-04-20 15:45:23 +0530

    Update main.py

--- Assignment3/codes/main.py
+++ Assignment3/codes/main.py
@@ -1,1 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+#Input parameters from excel file 
+df= pd.read_excel(r'https://github.com/Varshini11024/AI1110-PRV/blob/main/Assignment3/Tables/Given_data.xlsx?raw=true')
+print(df)
+
+df['A'] = np.arange(300,1000,100)
+# Create bar plot
+plt.bar(df['A']+50, df['No.of neon lamps'], width =100, color='blue', ec= 'black',alpha = 0.5)
+
+# Add title
+plt.title('HISTOGRAM',fontsize=17);
+
+plt.ylabel('No.of neon lamps')
+plt.xlabel('Lifetime(in hrs)')
+plt.show()
+
+sum=0
+data = df['No.of neon lamps']
+
+#The lifetime greater than 700 hours is from the range 4-7 in the given frequency table
+for i in range(4,7):
+   sum += data[i] 
+
+print("No.of neon lamps that have a lifetime of more than 700 hours is", sum)
